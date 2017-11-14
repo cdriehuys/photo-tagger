fc7661c536badde3a002f3997e164c8c7ec4e659
Chathan Driehuys <chathan@driehuys.com>
2017-11-14 03:12:43 -0500

    Ensure subsequent saves update the existing image.

--- app/src/main/java/com/unc/driehuys/chathan/phototagger/MainActivity.java
+++ app/src/main/java/com/unc/driehuys/chathan/phototagger/MainActivity.java
@@ -1,6 +1,7 @@
 package com.unc.driehuys.chathan.phototagger;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.media.Image;
@@ -10,6 +11,7 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -22,6 +24,8 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_TAKE_PHOTO = 1;
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private int currentPhotoSize;
 
@@ -120,12 +124,30 @@ public class MainActivity extends AppCompatActivity {
     private void saveImage() {
         String tags = "";
 
-        Object[] options = {
-                currentPhotoPath,
-                currentPhotoSize,
-                tags,
-        };
+        Cursor c = db.rawQuery("SELECT path FROM photos WHERE path = ?", new String[] {currentPhotoPath});
 
-        db.execSQL("INSERT INTO photos VALUES (?, ?, ?)", options);
+        if (c.getCount() == 0) {
+            db.execSQL(
+                    "INSERT INTO photos VALUES (?, ?, ?)",
+                    new Object[] {
+                            currentPhotoPath,
+                            currentPhotoSize,
+                            tags,
+                    });
+
+            Log.v(LOG_TAG, "Inserted new photo: " + currentPhotoPath);
+        } else {
+            db.execSQL(
+                    "UPDATE photos SET tags = ?, size = ? WHERE path = ?",
+                    new Object[] {
+                            tags,
+                            currentPhotoSize,
+                            currentPhotoPath,
+                    });
+
+            Log.v(LOG_TAG, "Updated photo at: " + currentPhotoPath);
+        }
+
+        c.close();
     }
 }
