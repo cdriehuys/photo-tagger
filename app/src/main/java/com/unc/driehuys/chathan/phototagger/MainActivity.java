cc1813c4d64ef27c3c9f294246cfa37288d1c12e
Chathan Driehuys <chathan@driehuys.com>
2017-11-14 02:10:29 -0500

    Add basic UI structure.

--- app/src/main/java/com/unc/driehuys/chathan/phototagger/MainActivity.java
+++ app/src/main/java/com/unc/driehuys/chathan/phototagger/MainActivity.java
@@ -1,13 +1,24 @@
 package com.unc.driehuys.chathan.phototagger;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+
+    public void onClick(View view) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = openOrCreateDatabase("phototagger.db", MODE_PRIVATE, null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS photos (path TEXT, size INTEGER, tags TEXT);");
     }
 }
